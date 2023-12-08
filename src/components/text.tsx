c9a1e619206acd4771b90faf6d19188070f1d499
Jaziel Rodriguez <aguafapolla@gmail.com>
2023-12-07 21:09:23 -0600

    framer motion added

---
+++ src/components/text.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+const Text: React.FC = () => {
+  return <Typewriter words={["Store", "Upload", "Download"]} loop />;
+};
+export default Text;
